1c59a95921525b365374039304def527492cfb3e
Cocobarce <155199739+CocoBarce@users.noreply.github.com>
2026-06-04 10:23:44 -0300

    Agrege lo de tinkercad

---
+++ stunning_bombul_robo1.ino
@@ -0,0 +1,14 @@
+const int pot = A5;
+const int motor = 10;
+
+void setup() {
+  pinMode(motor, OUTPUT);
+}
+
+void loop() {
+  int valorPot = analogRead(pot); 
+
+  int velocidad = map(valorPot, 0, 1023, 0, 255);
+
+  analogWrite(motor, velocidad);
+}
